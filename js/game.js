a7d6c0934c5a6e9f40124f3cdd8aadc61d2924a0
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-07-03 20:13:35 +0700

    HaiZuka

--- js/game.js
+++ js/game.js
@@ -1,10 +1,13 @@
 let game_W = 20;
 let game_H = 20;
 let XXX = 0, YYY = 0;
+let R = 0, r = 0;
+let drag = false;
+let d = false;
 let angle = 90;
 let ChAngle = -1;
 var bg = new Image();
-bg.src="images/background.jpg";
+bg.src="images/background.png";
 
 class game {
     constructor() {
@@ -40,7 +43,10 @@ class game {
 
     listenKeyboard() {
         document.addEventListener("keydown", key => {
-            
+            if (!drag) {
+                drag = true;
+                d = true;
+            }
         })
     }
 
@@ -52,36 +58,50 @@ class game {
 
     update() {
         this.render();
-        angle += ChAngle;
-        if (angle >= 180 || angle <= 0)
-            ChAngle = -ChAngle;
+        if (!drag) {
+            angle += ChAngle;
+            if (angle >= 180 || angle <= 0)
+                ChAngle = -ChAngle;
+        } else {
+            if (r < 10 * this.getWidth() && d)
+                r += 10;
+            else {
+                d = false;
+                r -= 10;
+            }
+            if (r < R) {
+                r = R;
+                drag = false;
+            }
+        }
     }
 
     render() {
-        this.canvas.width = document.documentElement.clientWidth;
-        this.canvas.height = document.documentElement.clientHeight;
-        game_W = this.canvas.width;
-        game_H = this.canvas.height;
-        XXX = game_W / 2;
+        if (game_W != document.documentElement.clientWidth || game_H != document.documentElement.clientHeight) {
+            this.canvas.width = document.documentElement.clientWidth;
+            this.canvas.height = document.documentElement.clientHeight;
+            game_W = this.canvas.width;
+            game_H = this.canvas.height;
+            XXX = game_W / 2;
+            YYY = game_H * 0.185;
+            R = r = this.getWidth() * 2;
+        }
     }
 
     draw() {
         this.clearScreen();
 
         this.context.beginPath();
-        this.context.strokeStyle  = "#00FF00";
+        this.context.strokeStyle  = "#000000";
         this.context.lineWidth = Math.floor(this.getWidth() / 10);
         this.context.moveTo(XXX, YYY);
-        this.context.lineTo(XXX + 100 * Math.cos(this.toRadius(angle)), YYY + 100 * Math.sin(this.toRadius(angle)));
+        this.context.lineTo(XXX + r * Math.cos(this.toRadius(angle)), YYY + r * Math.sin(this.toRadius(angle)));
         this.context.stroke();
     }
 
     clearScreen() {
         this.context.clearRect(0, 0, game_W, game_H);
-        if (bg.width / game_W < bg.height / game_H)
-            this.context.drawImage(bg, 0, (bg.height - game_H * (bg.width / game_W)) / 2, bg.width, game_H * (bg.width / game_W), 0, 0, game_W, game_H);
-        else
-            this.context.drawImage(bg, (bg.width - game_W * (bg.height / game_H)) / 2, 0, game_W * (bg.height / game_H), bg.height, 0, 0, game_W, game_H);
+        this.context.drawImage(bg, (bg.width - game_W * (bg.height / game_H)) / 2, 0, game_W * (bg.height / game_H), bg.height, 0, 0, game_W, game_H);
     }
 
     getWidth() {
